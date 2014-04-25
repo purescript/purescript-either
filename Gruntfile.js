0e76a2718e97d20b6c6ac46c039028399d87e1fe
Gary Burgess <gary.burgess@gmail.com>
2014-04-25 22:04:56 +0100

    Remove test-related stuff (moved to core-tests)

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,36 +11,17 @@ module.exports = function(grunt) {
     
     clean: {
       dedupe: ["bower_components/purescript-either"],
-      tests: ["tmp"],
       lib: ["js", "externs"]
     },
   
     pscMake: ["<%=libFiles%>"],
-    dotPsci: ["<%=libFiles%>"],
-  
-    psc: {
-      tests: {
-        options: {
-          module: ["Main"],
-          main: true
-        },
-        src: ["tests/Tests.purs", "<%=libFiles%>"],
-        dest: "tmp/tests.js"
-      }
-    },
+    dotPsci: ["<%=libFiles%>"]
     
-    execute: {
-      tests: {
-        src: "tmp/tests.js"
-      }
-    }      
   });
 
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-purescript");
-  grunt.loadNpmTasks("grunt-execute");
   
-  grunt.registerTask("test", ["clean:dedupe", "clean:tests", "psc:tests", "execute:tests"]);
   grunt.registerTask("make", ["clean:dedupe", "pscMake", "dotPsci"]);
-  grunt.registerTask("default", ["test", "make"]);
+  grunt.registerTask("default", ["make"]);
 };
