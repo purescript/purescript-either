7cad3cdd956fdb1f1854c162d5212b8fe867d9f9
Eric Easley <escalier@riseup.net>
2015-03-18 15:11:20 -0400

    Comments and generated docs

--- src/Data/Either/Unsafe.purs
+++ src/Data/Either/Unsafe.purs
@@ -2,8 +2,13 @@ module Data.Either.Unsafe where
 
 import Data.Either
 
+-- | A partial function that extracts the value from the `Left` data constructor.
+-- | Passing a `Right` to `fromLeft` will throw an error at runtime.
+fromLeft :: forall a b. Either a b -> a
+fromLeft (Left a) = a
+
+-- | A partial function that extracts the value from the `Right` data constructor.
+-- | Passing a `Left` to `fromRight` will throw an error at runtime.
 fromRight :: forall a b. Either a b -> b
 fromRight (Right a) = a
 
-fromLeft :: forall a b. Either a b -> a
-fromLeft (Left a) = a
