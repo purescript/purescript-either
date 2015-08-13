6095ecc5e4af06db2230a64fe266a1e182b1691d
Gary Burgess <gary.burgess@gmail.com>
2015-08-13 14:20:26 +0100

    Fix warnings about partial functions

--- src/Data/Either/Unsafe.purs
+++ src/Data/Either/Unsafe.purs
@@ -1,4 +1,7 @@
-module Data.Either.Unsafe where
+module Data.Either.Unsafe
+  ( fromLeft
+  , fromRight
+  ) where
 
 import Prelude
 
@@ -8,9 +11,12 @@ import Data.Either
 -- | Passing a `Right` to `fromLeft` will throw an error at runtime.
 fromLeft :: forall a b. Either a b -> a
 fromLeft (Left a) = a
+fromLeft _ = unsafeThrow "Data.Either.Unsafe.fromLeft called on Right value"
 
 -- | A partial function that extracts the value from the `Right` data constructor.
 -- | Passing a `Left` to `fromRight` will throw an error at runtime.
 fromRight :: forall a b. Either a b -> b
 fromRight (Right a) = a
+fromRight _ = unsafeThrow "Data.Either.Unsafe.fromLeft called on Left value"
 
+foreign import unsafeThrow :: forall a. String -> a
