0255fb291af4599887a9a8daa369d9c9d3841e11
Nathan Faubion <nathan@n-son.com>
2016-07-13 20:38:41 -0500

    Replace Data.Either.Nested

--- src/Data/Either/Nested.purs
+++ src/Data/Either/Nested.purs
@@ -12,15 +12,25 @@
 -- | toEither3 = either3 Red Green Blue
 -- |
 -- | fromEither3 :: Color -> Either3 Number Number Number
--- | fromEither3 (Red   v) = either1of3 v
--- | fromEither3 (Green v) = either2of3 v
--- | fromEither3 (Blue  v) = either3of3 v
+-- | fromEither3 (Red   v) = in3 v
+-- | fromEither3 (Green v) = in2 v
+-- | fromEither3 (Blue  v) = in1 v
 -- | ```
-module Data.Either.Nested where
+-- |
+-- | Note that the numbering counts from right-to-left. This is because the
+-- | nesting is left-associative. Nesting this way preserves useful instances.
+module Data.Either.Nested
+  ( in1, in2, in3, in4, in5, in6, in7, in8, in9, in10
+  , at1, at2, at3, at4, at5, at6, at7, at8, at9, at10
+  , Either1, Either2, Either3, Either4, Either5, Either6, Either7, Either8, Either9, Either10
+  , either1, either2, either3, either4, either5, either6, either7, either8, either9, either10
+  ) where
 
-import Data.Either (Either(..), either)
+import Data.Either (Either(..))
+import Data.Void (Void, absurd)
 
-type Either2 a z = Either a z
+type Either1 z = Either Void z
+type Either2 a z = Either (Either1 a) z
 type Either3 a b z = Either (Either2 a b) z
 type Either4 a b c z = Either (Either3 a b c) z
 type Either5 a b c d z = Either (Either4 a b c d) z
@@ -30,200 +40,233 @@ type Either8 a b c d e f g z = Either (Either7 a b c d e f g) z
 type Either9 a b c d e f g h z = Either (Either8 a b c d e f g h) z
 type Either10 a b c d e f g h i z = Either (Either9 a b c d e f g h i) z
 
--- Either2
-either1of2 :: forall a b. a -> Either2 a b
-either1of2 = Left
+type E2 z a = Either z a
+type E3 z a b = Either (E2 z a) b
+type E4 z a b c = Either (E3 z a b) c
+type E5 z a b c d = Either (E4 z a b c) d
+type E6 z a b c d e = Either (E5 z a b c d) e
+type E7 z a b c d e f = Either (E6 z a b c d e) f
+type E8 z a b c d e f g = Either (E7 z a b c d e f) g
+type E9 z a b c d e f g h = Either (E8 z a b c d e f g) h
+type E10 z a b c d e f g h i = Either (E9 z a b c d e f g h) i
+type E11 z a b c d e f g h i j = Either (E10 z a b c d e f g h i) j
 
-either2of2 :: forall a b. b -> Either2 a b
-either2of2 = Right
+in1 :: forall z a. a -> E2 z a
+in1 = Right
 
--- Either3
-either1of3 :: forall a b c. a -> Either3 a b c
-either1of3 v = (Left (Left v))
+in2 :: forall z a b. a -> E3 z a b
+in2 v = Left (Right v)
 
-either2of3 :: forall a b c. b -> Either3 a b c
-either2of3 v = (Left (Right v))
+in3 :: forall z a b c. a -> E4 z a b c
+in3 v = Left (Left (Right v))
 
-either3of3 :: forall a b c. c -> Either3 a b c
-either3of3 v = Right v
+in4 :: forall z a b c d. a -> E5 z a b c d
+in4 v = Left (Left (Left (Right v)))
 
--- Either4
-either1of4 :: forall a b c d. a -> Either4 a b c d
-either1of4 v = (Left (Left (Left v)))
+in5 :: forall z a b c d e. a -> E6 z a b c d e
+in5 v = Left (Left (Left (Left (Right v))))
 
-either2of4 :: forall a b c d. b -> Either4 a b c d
-either2of4 v = (Left (Left (Right v)))
+in6 :: forall z a b c d e f. a -> E7 z a b c d e f
+in6 v = Left (Left (Left (Left (Left (Right v)))))
 
-either3of4 :: forall a b c d. c -> Either4 a b c d
-either3of4 v = (Left (Right v))
+in7 :: forall z a b c d e f g. a -> E8 z a b c d e f g
+in7 v = Left (Left (Left (Left (Left (Left (Right v))))))
 
-either4of4 :: forall a b c d. d -> Either4 a b c d
-either4of4 v = Right v
+in8 :: forall z a b c d e f g h. a -> E9 z a b c d e f g h
+in8 v = Left (Left (Left (Left (Left (Left (Left (Right v)))))))
 
--- Either5
-either1of5 :: forall a b c d e. a -> Either5 a b c d e
-either1of5 v = (Left (Left (Left (Left v))))
+in9 :: forall z a b c d e f g h i. a -> E10 z a b c d e f g h i
+in9 v = Left (Left (Left (Left (Left (Left (Left (Left (Right v))))))))
 
-either2of5 :: forall a b c d e. b -> Either5 a b c d e
-either2of5 v = (Left (Left (Left (Right v))))
+in10 :: forall z a b c d e f g h i j. a -> E11 z a b c d e f g h i j
+in10 v = Left (Left (Left (Left (Left (Left (Left (Left (Left (Right v)))))))))
 
-either3of5 :: forall a b c d e. c -> Either5 a b c d e
-either3of5 v = (Left (Left (Right v)))
+at1 :: forall z r a. r -> (a -> r) -> E2 z a -> r
+at1 b a y = case y of
+  Right r -> a r
+  _ -> b
 
-either4of5 :: forall a b c d e. d -> Either5 a b c d e
-either4of5 v = (Left (Right v))
+at2 :: forall z r a b. r -> (a -> r) -> E3 z a b -> r
+at2 b a y = case y of
+  Left (Right r) -> a r
+  _ -> b
 
-either5of5 :: forall a b c d e. e -> Either5 a b c d e
-either5of5 v = Right v
+at3 :: forall z r a b c. r -> (a -> r) -> E4 z a b c -> r
+at3 b a y = case y of
+  Left (Left (Right r)) -> a r
+  _ -> b
 
--- Either6
-either1of6 :: forall a b c d e f. a -> Either6 a b c d e f
-either1of6 v = (Left (Left (Left (Left (Left v)))))
+at4 :: forall z r a b c d. r -> (a -> r) -> E5 z a b c d -> r
+at4 b a y = case y of
+  Left (Left (Left (Right r))) -> a r
+  _ -> b
 
-either2of6 :: forall a b c d e f. b -> Either6 a b c d e f
-either2of6 v = (Left (Left (Left (Left (Right v)))))
+at5 :: forall z r a b c d e. r -> (a -> r) -> E6 z a b c d e -> r
+at5 b a y = case y of
+  Left (Left (Left (Left (Right r)))) -> a r
+  _ -> b
 
-either3of6 :: forall a b c d e f. c -> Either6 a b c d e f
-either3of6 v = (Left (Left (Left (Right v))))
+at6 :: forall z r a b c d e f. r -> (a -> r) -> E7 z a b c d e f -> r
+at6 b a y = case y of
+  Left (Left (Left (Left (Left (Right r))))) -> a r
+  _ -> b
 
-either4of6 :: forall a b c d e f. d -> Either6 a b c d e f
-either4of6 v = (Left (Left (Right v)))
+at7 :: forall z r a b c d e f g. r -> (a -> r) -> E8 z a b c d e f g -> r
+at7 b a y = case y of
+  Left (Left (Left (Left (Left (Left (Right r)))))) -> a r
+  _ -> b
 
-either5of6 :: forall a b c d e f. e -> Either6 a b c d e f
-either5of6 v = (Left (Right v))
+at8 :: forall z r a b c d e f g h. r -> (a -> r) -> E9 z a b c d e f g h -> r
+at8 b a y = case y of
+  Left (Left (Left (Left (Left (Left (Left (Right r))))))) -> a r
+  _ -> b
 
-either6of6 :: forall a b c d e f. f -> Either6 a b c d e f
-either6of6 v = Right v
+at9 :: forall z r a b c d e f g h i. r -> (a -> r) -> E10 z a b c d e f g h i -> r
+at9 b a y = case y of
+  Left (Left (Left (Left (Left (Left (Left (Left (Right r)))))))) -> a r
+  _ -> b
 
--- Either7
-either1of7 :: forall a b c d e f g. a -> Either7 a b c d e f g
-either1of7 v = (Left (Left (Left (Left (Left (Left v))))))
+at10 :: forall z r a b c d e f g h i j. r -> (a -> r) -> E11 z a b c d e f g h i j -> r
+at10 b a y = case y of
+  Left (Left (Left (Left (Left (Left (Left (Left (Left (Right r))))))))) -> a r
+  _ -> b
 
-either2of7 :: forall a b c d e f g. b -> Either7 a b c d e f g
-either2of7 v = (Left (Left (Left (Left (Left (Right v))))))
+either1 :: forall a. Either1 a -> a
+either1 y = case y of
+  Right r -> r
+  Left _0 -> absurd _0
 
-either3of7 :: forall a b c d e f g. c -> Either7 a b c d e f g
-either3of7 v = (Left (Left (Left (Left (Right v)))))
+either2 :: forall r a b. (a -> r) -> (b -> r) -> Either2 a b -> r
+either2 a b y = case y of
+  Right r -> b r
+  Left _1 -> case _1 of
+    Right r -> a r
+    Left _0 -> absurd _0
 
-either4of7 :: forall a b c d e f g. d -> Either7 a b c d e f g
-either4of7 v = (Left (Left (Left (Right v))))
+either3 :: forall r a b c. (a -> r) -> (b -> r) -> (c -> r) -> Either3 a b c -> r
+either3 a b c y = case y of
+  Right r -> c r
+  Left _2 -> case _2 of
+    Right r -> b r
+    Left _1 -> case _1 of
+      Right r -> a r
+      Left _0 -> absurd _0
 
-either5of7 :: forall a b c d e f g. e -> Either7 a b c d e f g
-either5of7 v = (Left (Left (Right v)))
+either4 :: forall r a b c d. (a -> r) -> (b -> r) -> (c -> r) -> (d -> r) -> Either4 a b c d -> r
+either4 a b c d y = case y of
+  Right r -> d r
+  Left _3 -> case _3 of
+    Right r -> c r
+    Left _2 -> case _2 of
+      Right r -> b r
+      Left _1 -> case _1 of
+        Right r -> a r
+        Left _0 -> absurd _0
 
-either6of7 :: forall a b c d e f g. f -> Either7 a b c d e f g
-either6of7 v = (Left (Right v))
+either5 :: forall r a b c d e. (a -> r) -> (b -> r) -> (c -> r) -> (d -> r) -> (e -> r) -> Either5 a b c d e -> r
+either5 a b c d e y = case y of
+  Right r -> e r
+  Left _4 -> case _4 of
+    Right r -> d r
+    Left _3 -> case _3 of
+      Right r -> c r
+      Left _2 -> case _2 of
+        Right r -> b r
+        Left _1 -> case _1 of
+          Right r -> a r
+          Left _0 -> absurd _0
 
-either7of7 :: forall a b c d e f g. g -> Either7 a b c d e f g
-either7of7 v = Right v
+either6 :: forall r a b c d e f. (a -> r) -> (b -> r) -> (c -> r) -> (d -> r) -> (e -> r) -> (f -> r) -> Either6 a b c d e f -> r
+either6 a b c d e f y = case y of
+  Right r -> f r
+  Left _5 -> case _5 of
+    Right r -> e r
+    Left _4 -> case _4 of
+      Right r -> d r
+      Left _3 -> case _3 of
+        Right r -> c r
+        Left _2 -> case _2 of
+          Right r -> b r
+          Left _1 -> case _1 of
+            Right r -> a r
+            Left _0 -> absurd _0
 
--- Either8
-either1of8 :: forall a b c d e f g h. a -> Either8 a b c d e f g h
-either1of8 v = (Left (Left (Left (Left (Left (Left (Left v)))))))
+either7 :: forall r a b c d e f g. (a -> r) -> (b -> r) -> (c -> r) -> (d -> r) -> (e -> r) -> (f -> r) -> (g -> r) -> Either7 a b c d e f g -> r
+either7 a b c d e f g y = case y of
+  Right r -> g r
+  Left _6 -> case _6 of
+    Right r -> f r
+    Left _5 -> case _5 of
+      Right r -> e r
+      Left _4 -> case _4 of
+        Right r -> d r
+        Left _3 -> case _3 of
+          Right r -> c r
+          Left _2 -> case _2 of
+            Right r -> b r
+            Left _1 -> case _1 of
+              Right r -> a r
+              Left _0 -> absurd _0
 
-either2of8 :: forall a b c d e f g h. b -> Either8 a b c d e f g h
-either2of8 v = (Left (Left (Left (Left (Left (Left (Right v)))))))
+either8 :: forall r a b c d e f g h. (a -> r) -> (b -> r) -> (c -> r) -> (d -> r) -> (e -> r) -> (f -> r) -> (g -> r) -> (h -> r) -> Either8 a b c d e f g h -> r
+either8 a b c d e f g h y = case y of
+  Right r -> h r
+  Left _7 -> case _7 of
+    Right r -> g r
+    Left _6 -> case _6 of
+      Right r -> f r
+      Left _5 -> case _5 of
+        Right r -> e r
+        Left _4 -> case _4 of
+          Right r -> d r
+          Left _3 -> case _3 of
+            Right r -> c r
+            Left _2 -> case _2 of
+              Right r -> b r
+              Left _1 -> case _1 of
+                Right r -> a r
+                Left _0 -> absurd _0
 
-either3of8 :: forall a b c d e f g h. c -> Either8 a b c d e f g h
-either3of8 v = (Left (Left (Left (Left (Left (Right v))))))
+either9 :: forall r a b c d e f g h i. (a -> r) -> (b -> r) -> (c -> r) -> (d -> r) -> (e -> r) -> (f -> r) -> (g -> r) -> (h -> r) -> (i -> r) -> Either9 a b c d e f g h i -> r
+either9 a b c d e f g h i y = case y of
+  Right r -> i r
+  Left _8 -> case _8 of
+    Right r -> h r
+    Left _7 -> case _7 of
+      Right r -> g r
+      Left _6 -> case _6 of
+        Right r -> f r
+        Left _5 -> case _5 of
+          Right r -> e r
+          Left _4 -> case _4 of
+            Right r -> d r
+            Left _3 -> case _3 of
+              Right r -> c r
+              Left _2 -> case _2 of
+                Right r -> b r
+                Left _1 -> case _1 of
+                  Right r -> a r
+                  Left _0 -> absurd _0
 
-either4of8 :: forall a b c d e f g h. d -> Either8 a b c d e f g h
-either4of8 v = (Left (Left (Left (Left (Right v)))))
-
-either5of8 :: forall a b c d e f g h. e -> Either8 a b c d e f g h
-either5of8 v = (Left (Left (Left (Right v))))
-
-either6of8 :: forall a b c d e f g h. f -> Either8 a b c d e f g h
-either6of8 v = (Left (Left (Right v)))
-
-either7of8 :: forall a b c d e f g h. g -> Either8 a b c d e f g h
-either7of8 v = (Left (Right v))
-
-either8of8 :: forall a b c d e f g h. h -> Either8 a b c d e f g h
-either8of8 v = Right v
-
--- Either9
-either1of9 :: forall a b c d e f g h i. a -> Either9 a b c d e f g h i
-either1of9 v = (Left (Left (Left (Left (Left (Left (Left (Left v))))))))
-
-either2of9 :: forall a b c d e f g h i. b -> Either9 a b c d e f g h i
-either2of9 v = (Left (Left (Left (Left (Left (Left (Left (Right v))))))))
-
-either3of9 :: forall a b c d e f g h i. c -> Either9 a b c d e f g h i
-either3of9 v = (Left (Left (Left (Left (Left (Left (Right v)))))))
-
-either4of9 :: forall a b c d e f g h i. d -> Either9 a b c d e f g h i
-either4of9 v = (Left (Left (Left (Left (Left (Right v))))))
-
-either5of9 :: forall a b c d e f g h i. e -> Either9 a b c d e f g h i
-either5of9 v = (Left (Left (Left (Left (Right v)))))
-
-either6of9 :: forall a b c d e f g h i. f -> Either9 a b c d e f g h i
-either6of9 v = (Left (Left (Left (Right v))))
-
-either7of9 :: forall a b c d e f g h i. g -> Either9 a b c d e f g h i
-either7of9 v = (Left (Left (Right v)))
-
-either8of9 :: forall a b c d e f g h i. h -> Either9 a b c d e f g h i
-either8of9 v = (Left (Right v))
-
-either9of9 :: forall a b c d e f g h i. i -> Either9 a b c d e f g h i
-either9of9 v = Right v
-
--- Either10
-either1of10 :: forall a b c d e f g h i j. a -> Either10 a b c d e f g h i j
-either1of10 v = (Left (Left (Left (Left (Left (Left (Left (Left (Left v)))))))))
-
-either2of10 :: forall a b c d e f g h i j. b -> Either10 a b c d e f g h i j
-either2of10 v = (Left (Left (Left (Left (Left (Left (Left (Left (Right v)))))))))
-
-either3of10 :: forall a b c d e f g h i j. c -> Either10 a b c d e f g h i j
-either3of10 v = (Left (Left (Left (Left (Left (Left (Left (Right v))))))))
-
-either4of10 :: forall a b c d e f g h i j. d -> Either10 a b c d e f g h i j
-either4of10 v = (Left (Left (Left (Left (Left (Left (Right v)))))))
-
-either5of10 :: forall a b c d e f g h i j. e -> Either10 a b c d e f g h i j
-either5of10 v = (Left (Left (Left (Left (Left (Right v))))))
-
-either6of10 :: forall a b c d e f g h i j. f -> Either10 a b c d e f g h i j
-either6of10 v = (Left (Left (Left (Left (Right v)))))
-
-either7of10 :: forall a b c d e f g h i j. g -> Either10 a b c d e f g h i j
-either7of10 v = (Left (Left (Left (Right v))))
-
-either8of10 :: forall a b c d e f g h i j. h -> Either10 a b c d e f g h i j
-either8of10 v = (Left (Left (Right v)))
-
-either9of10 :: forall a b c d e f g h i j. i -> Either10 a b c d e f g h i j
-either9of10 v = (Left (Right v))
-
-either10of10 :: forall a b c d e f g h i j. j -> Either10 a b c d e f g h i j
-either10of10 v = Right v
-
-either2 :: forall a b z. (a -> z) -> (b -> z) -> Either2 a b -> z
-either2 = either
-
-either3 :: forall a b c z. (a -> z) -> (b -> z) -> (c -> z) -> Either3 a b c -> z
-either3 a b z = either (either2 a b) z
-
-either4 :: forall a b c d z. (a -> z) -> (b -> z) -> (c -> z) -> (d -> z) -> Either4 a b c d -> z
-either4 a b c z = either (either3 a b c) z
-
-either5 :: forall a b c d e z. (a -> z) -> (b -> z) -> (c -> z) -> (d -> z) -> (e -> z) -> Either5 a b c d e -> z
-either5 a b c d z = either (either4 a b c d) z
-
-either6 :: forall a b c d e f z. (a -> z) -> (b -> z) -> (c -> z) -> (d -> z) -> (e -> z) -> (f -> z) -> Either6 a b c d e f -> z
-either6 a b c d e z = either (either5 a b c d e) z
-
-either7 :: forall a b c d e f g z. (a -> z) -> (b -> z) -> (c -> z) -> (d -> z) -> (e -> z) -> (f -> z) -> (g -> z) -> Either7 a b c d e f g -> z
-either7 a b c d e f z = either (either6 a b c d e f) z
-
-either8 :: forall a b c d e f g h z. (a -> z) -> (b -> z) -> (c -> z) -> (d -> z) -> (e -> z) -> (f -> z) -> (g -> z) -> (h -> z) -> Either8 a b c d e f g h -> z
-either8 a b c d e f g z = either (either7 a b c d e f g) z
-
-either9 :: forall a b c d e f g h i z. (a -> z) -> (b -> z) -> (c -> z) -> (d -> z) -> (e -> z) -> (f -> z) -> (g -> z) -> (h -> z) -> (i -> z) -> Either9 a b c d e f g h i -> z
-either9 a b c d e f g h z = either (either8 a b c d e f g h) z
-
-either10 :: forall a b c d e f g h i j z. (a -> z) -> (b -> z) -> (c -> z) -> (d -> z) -> (e -> z) -> (f -> z) -> (g -> z) -> (h -> z) -> (i -> z) -> (j -> z) -> Either10 a b c d e f g h i j -> z
-either10 a b c d e f g h i z = either (either9 a b c d e f g h i) z
+either10 :: forall r a b c d e f g h i j. (a -> r) -> (b -> r) -> (c -> r) -> (d -> r) -> (e -> r) -> (f -> r) -> (g -> r) -> (h -> r) -> (i -> r) -> (j -> r) -> Either10 a b c d e f g h i j -> r
+either10 a b c d e f g h i j y = case y of
+  Right r -> j r
+  Left _9 -> case _9 of
+    Right r -> i r
+    Left _8 -> case _8 of
+      Right r -> h r
+      Left _7 -> case _7 of
+        Right r -> g r
+        Left _6 -> case _6 of
+          Right r -> f r
+          Left _5 -> case _5 of
+            Right r -> e r
+            Left _4 -> case _4 of
+              Right r -> d r
+              Left _3 -> case _3 of
+                Right r -> c r
+                Left _2 -> case _2 of
+                  Right r -> b r
+                  Left _1 -> case _1 of
+                    Right r -> a r
+                    Left _0 -> absurd _0
