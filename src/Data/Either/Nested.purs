6227fcc966345716535b4722f7ced7fb91bfbf24
Gary Burgess <gary.burgess@gmail.com>
2018-05-13 17:49:15 +0100

    Add (\/) type operator for Either

--- src/Data/Either/Nested.purs
+++ src/Data/Either/Nested.purs
@@ -17,114 +17,105 @@
 -- | toEither3 (Blue  v) = in3 v
 -- | ```
 module Data.Either.Nested
-  ( in1, in2, in3, in4, in5, in6, in7, in8, in9, in10
+  ( type (\/)
+  , in1, in2, in3, in4, in5, in6, in7, in8, in9, in10
   , at1, at2, at3, at4, at5, at6, at7, at8, at9, at10
   , Either1, Either2, Either3, Either4, Either5, Either6, Either7, Either8, Either9, Either10
   , either1, either2, either3, either4, either5, either6, either7, either8, either9, either10
-  , E2, E3, E4, E5, E6, E7, E8, E9, E10, E11
   ) where
 
 import Data.Either (Either(..))
 import Data.Void (Void, absurd)
 
-type Either1 a = E2 a Void
-type Either2 a b = E3 a b Void
-type Either3 a b c = E4 a b c Void
-type Either4 a b c d = E5 a b c d Void
-type Either5 a b c d e = E6 a b c d e Void
-type Either6 a b c d e f = E7 a b c d e f Void
-type Either7 a b c d e f g = E8 a b c d e f g Void
-type Either8 a b c d e f g h = E9 a b c d e f g h Void
-type Either9 a b c d e f g h i = E10 a b c d e f g h i Void
-type Either10 a b c d e f g h i j = E11 a b c d e f g h i j Void
+infixr 6 type Either as \/
 
-type E2 a z = Either a z
-type E3 a b z = Either a (E2 b z)
-type E4 a b c z = Either a (E3 b c z)
-type E5 a b c d z = Either a (E4 b c d z)
-type E6 a b c d e z = Either a (E5 b c d e z)
-type E7 a b c d e f z = Either a (E6 b c d e f z)
-type E8 a b c d e f g z = Either a (E7 b c d e f g z)
-type E9 a b c d e f g h z = Either a (E8 b c d e f g h z)
-type E10 a b c d e f g h i z = Either a (E9 b c d e f g h i z)
-type E11 a b c d e f g h i j z = Either a (E10 b c d e f g h i j z)
+type Either1 a = a \/ Void
+type Either2 a b = a \/ b \/ Void
+type Either3 a b c = a \/ b \/ c \/ Void
+type Either4 a b c d = a \/ b \/ c \/ d \/ Void
+type Either5 a b c d e = a \/ b \/ c \/ d \/ e \/ Void
+type Either6 a b c d e f = a \/ b \/ c \/ d \/ e \/ f \/ Void
+type Either7 a b c d e f g = a \/ b \/ c \/ d \/ e \/ f \/ g \/ Void
+type Either8 a b c d e f g h = a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ Void
+type Either9 a b c d e f g h i = a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ i \/ Void
+type Either10 a b c d e f g h i j = a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ i \/ j \/ Void
 
-in1 :: forall a z. a -> E2 a z
+in1 :: forall a z. a -> a \/ z
 in1 = Left
 
-in2 :: forall a b z. b -> E3 a b z
+in2 :: forall a b z. b -> a \/ b \/ z
 in2 v = Right (Left v)
 
-in3 :: forall a b c z. c -> E4 a b c z
+in3 :: forall a b c z. c -> a \/ b \/ c \/ z
 in3 v = Right (Right (Left v))
 
-in4 :: forall a b c d z. d -> E5 a b c d z
+in4 :: forall a b c d z. d -> a \/ b \/ c \/ d \/ z
 in4 v = Right (Right (Right (Left v)))
 
-in5 :: forall a b c d e z. e -> E6 a b c d e z
+in5 :: forall a b c d e z. e -> a \/ b \/ c \/ d \/ e \/ z
 in5 v = Right (Right (Right (Right (Left v))))
 
-in6 :: forall a b c d e f z. f -> E7 a b c d e f z
+in6 :: forall a b c d e f z. f -> a \/ b \/ c \/ d \/ e \/ f \/ z
 in6 v = Right (Right (Right (Right (Right (Left v)))))
 
-in7 :: forall a b c d e f g z. g -> E8 a b c d e f g z
+in7 :: forall a b c d e f g z. g -> a \/ b \/ c \/ d \/ e \/ f \/ g \/ z
 in7 v = Right (Right (Right (Right (Right (Right (Left v))))))
 
-in8 :: forall a b c d e f g h z. h -> E9 a b c d e f g h z
+in8 :: forall a b c d e f g h z. h -> a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ z
 in8 v = Right (Right (Right (Right (Right (Right (Right (Left v)))))))
 
-in9 :: forall a b c d e f g h i z. i -> E10 a b c d e f g h i z
+in9 :: forall a b c d e f g h i z. i -> a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ i \/ z
 in9 v = Right (Right (Right (Right (Right (Right (Right (Right (Left v))))))))
 
-in10 :: forall a b c d e f g h i j z. j -> E11 a b c d e f g h i j z
+in10 :: forall a b c d e f g h i j z. j -> a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ i \/ j \/ z
 in10 v = Right (Right (Right (Right (Right (Right (Right (Right (Right (Left v)))))))))
 
-at1 :: forall r a z. r -> (a -> r) -> E2 a z -> r
+at1 :: forall r a z. r -> (a -> r) -> a \/ z -> r
 at1 b f y = case y of
   Left r -> f r
   _ -> b
 
-at2 :: forall r a b z. r -> (b -> r) -> E3 a b z -> r
+at2 :: forall r a b z. r -> (b -> r) -> a \/ b \/ z -> r
 at2 b f y = case y of
   Right (Left r) -> f r
   _ -> b
 
-at3 :: forall r a b c z. r -> (c -> r) -> E4 a b c z -> r
+at3 :: forall r a b c z. r -> (c -> r) -> a \/ b \/ c \/ z -> r
 at3 b f y = case y of
   Right (Right (Left r)) -> f r
   _ -> b
 
-at4 :: forall r a b c d z. r -> (d -> r) -> E5 a b c d z -> r
+at4 :: forall r a b c d z. r -> (d -> r) -> a \/ b \/ c \/ d \/ z -> r
 at4 b f y = case y of
   Right (Right (Right (Left r))) -> f r
   _ -> b
 
-at5 :: forall r a b c d e z. r -> (e -> r) -> E6 a b c d e z -> r
+at5 :: forall r a b c d e z. r -> (e -> r) -> a \/ b \/ c \/ d \/ e \/ z -> r
 at5 b f y = case y of
   Right (Right (Right (Right (Left r)))) -> f r
   _ -> b
 
-at6 :: forall r a b c d e f z. r -> (f -> r) -> E7 a b c d e f z -> r
+at6 :: forall r a b c d e f z. r -> (f -> r) -> a \/ b \/ c \/ d \/ e \/ f \/ z -> r
 at6 b f y = case y of
   Right (Right (Right (Right (Right (Left r))))) -> f r
   _ -> b
 
-at7 :: forall r a b c d e f g z. r -> (g -> r) -> E8 a b c d e f g z -> r
+at7 :: forall r a b c d e f g z. r -> (g -> r) -> a \/ b \/ c \/ d \/ e \/ f \/ g \/ z -> r
 at7 b f y = case y of
   Right (Right (Right (Right (Right (Right (Left r)))))) -> f r
   _ -> b
 
-at8 :: forall r a b c d e f g h z. r -> (h -> r) -> E9 a b c d e f g h z -> r
+at8 :: forall r a b c d e f g h z. r -> (h -> r) -> a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ z -> r
 at8 b f y = case y of
   Right (Right (Right (Right (Right (Right (Right (Left r))))))) -> f r
   _ -> b
 
-at9 :: forall r a b c d e f g h i z. r -> (i -> r) -> E10 a b c d e f g h i z -> r
+at9 :: forall r a b c d e f g h i z. r -> (i -> r) -> a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ i \/ z -> r
 at9 b f y = case y of
   Right (Right (Right (Right (Right (Right (Right (Right (Left r)))))))) -> f r
   _ -> b
 
-at10 :: forall r a b c d e f g h i j z. r -> (j -> r) -> E11 a b c d e f g h i j z -> r
+at10 :: forall r a b c d e f g h i j z. r -> (j -> r) -> a \/ b \/ c \/ d \/ e \/ f \/ g \/ h \/ i \/ j \/ z -> r
 at10 b f y = case y of
   Right (Right (Right (Right (Right (Right (Right (Right (Right (Left r))))))))) -> f r
   _ -> b
